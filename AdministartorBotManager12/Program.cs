d2e8ae9fe00f536fcc3daff0d7e4a713268264ad
Razmik <r.anakhasyan@bangits.com>
2024-01-04 19:07:32 +0400

    Добавьте файлы проекта.

---
+++ AdministartorBotManager12/Program.cs
@@ -0,0 +1,26 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    _ = app.UseSwagger();
+    _ = app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
